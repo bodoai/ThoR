84c3ee305cd07ec01f1e5844fb2dc958cb8fdcb5
Andreas Mayer <Andreas.b.Mayer@student.hs-rm.de>
2024-12-30 08:19:48 +0100

    import test

---
+++ ThoR/Test/Alloy/t01a_section_import.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2024 RheinMain University of Applied Sciences
+Released under license as described in the file LICENSE.
+Authors: s. file CONTRIBUTORS
+-/
+
+/-
+Tests if you can import (and create)
+defined alloy blocks from other files
+-/
+
+
+import ThoR
+
+import ThoR.Test.Alloy.t01_section
+
+-- from t01_section
+create default2
