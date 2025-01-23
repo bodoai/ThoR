fc7507cde9907576b693d22831c299752a65b045
Andreas Mayer <Andreas.Mayer@hs-rm.de>
2025-01-23 11:18:26 +0100

    commandDecl can inculde itself (to represent predicate calls)

--- ThoR/Alloy/SymbolTable/commandDecl.lean
+++ ThoR/Alloy/SymbolTable/commandDecl.lean
@@ -5,6 +5,7 @@ Authors: s. file CONTRIBUTORS
 -/
 import ThoR.Shared.Syntax.Formula.formula
 import ThoR.Alloy.Syntax.Predicate.PredArg.predArg
+import ThoR.Alloy.SymbolTable.varDecl
 
 open Shared
 
@@ -14,38 +15,46 @@ namespace Alloy
   This structure represents a (Lean) command declaration
   of either an definition (def) or axiom.
   -/
-  structure commandDecl where
-    mk :: (name : String)
+  inductive commandDecl where
+    | mk  (name : String)
           (args : List (predArg) := []) -- empty if axiom
           (formulas : List (formula)) -- formulas in an Alloy pred or an Alloy fact
           (requiredDefs : List (String)) -- only for Lean Infoview
           (requiredVars : List (String)) -- only for Lean Infoview
           (predCalls : List (List (String))) -- called predicates
-          (relationCalls : List (String)) -- called relations
-          (signatureCalls : List (String)) -- called signatures
+          (relationCalls : List (varDecl)) -- called relations
+          (signatureCalls : List (commandDecl × List (varDecl))) -- called signatures
   deriving Repr
-
-  instance : Inhabited commandDecl where
-    default := {
-      name := default,
-      args := default,
-      formulas := default,
-      requiredDefs := default,
-      requiredVars := default,
-      predCalls := default,
-      relationCalls := default,
-      signatureCalls := default
-    }
-
   namespace commandDecl
+
+    def name | mk n _ _ _ _ _ _ _ => n
+    def args | mk _ args _ _ _ _ _ _ => args
+    def formulas | mk _ _ formulas _ _ _ _ _ => formulas
+    def requiredDefs | mk _ _ _ requiredDefs _ _ _ _ => requiredDefs
+    def requiredVars | mk _ _ _ _ requiredVars _ _ _ => requiredVars
+    def predCalls | mk _ _ _ _ _ predCalls _ _ => predCalls
+    def relationCalls | mk _ _ _ _ _ _ relationCalls _ => relationCalls
+    def signatureCalls | mk _ _ _ _ _ _ _ signatureCalls => signatureCalls
+
+    instance : Inhabited commandDecl where
+      default :=
+        commandDecl.mk
+          (name := default)
+          (args := default)
+          (formulas := default)
+          (requiredDefs := default)
+          (requiredVars := default)
+          (predCalls := default)
+          (relationCalls := default)
+          (signatureCalls := default)
 
   /--
   Generates a String representation from the type.
-
   -/
-  def toString (cd : commandDecl) : String :=
-    /-
-    -/
+  partial def toString (cd : commandDecl) : String :=
+    let sigCallsString :=
+      cd.signatureCalls.map fun sc =>
+        s!"({toString sc.1} {sc.2})"
     s!"commandDeclaration : \{
       name := {cd.name},
       args := {cd.args},
@@ -53,7 +62,7 @@ namespace Alloy
       required variables := {cd.requiredVars},
       called predicates := {cd.predCalls},
       called relations := {cd.relationCalls},
-      called signatures := {cd.signatureCalls},
+      called signatures := {sigCallsString},
       formulas := {cd.formulas}
     }"
 
