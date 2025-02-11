ca6326063a4e609b5f18112d55a77b6e3ff0e1c5
igler <bodo.igler@hs-rm.de>
2025-02-11 11:36:23 +0100

    - subtypeC

--- ThoR/Relation/SubType.lean
+++ ThoR/Relation/SubType.lean
@@ -4,6 +4,7 @@ Released under license as described in the file LICENSE.
 Authors: s. file CONTRIBUTORS
 -/
 
+-- import ThoR
 import ThoR.Relation
 
 namespace ThoR
@@ -50,57 +51,26 @@ namespace ThoR
     - inheritance tree → type hierarchy -/
   variable {R : Type} [TupleSet R]
 
-  def subtypeC_same_arity  {arity : ℕ} (t1 t2 : RelType R arity)
-  := match t1 with
-    | RelType.sig m h => true
-    | RelType.unary_rel m r h1 h2 => true
-    | RelType.rel r h => true
-    |_ => false
-
-  def RelType_arity_cast (arity1 arity2 : ℕ) (t : RelType R arity1) (p : arity1 = arity2): (RelType R arity2) :=
-    p ▸ t
-
-  def subtypeC (t1 : RelType R arity1) {arity2 : ℕ} (t2 : RelType R arity2)
-  :=
-    if h : arity1 = arity2
-    then subtypeC_same_arity
-      t1
-      (RelType_arity_cast arity2 arity1 t2 (symm h))
-    else false
-
-  /- TODO proof -/
-  theorem subtypeC_subtypeP (t1 t2 : RelType R arity):
-      subtypeC t1 t2 → subtypeP t1 t2
-  := by sorry
-
-  theorem subtypeC_subtype (t1 t2 : RelType R arity):
-      subtypeC t1 t2 → subtype t1 t2
-  := by
-    intro h1
-    apply subtypeP_subtype
-    apply subtypeC_subtypeP
-    apply h1
-
   lemma castable {t1 : RelType R arity} (r : Rel t1) (t2 : RelType R arity):
-    subtypeC t1 t2 → r.relation ∷ t2 :=
+    subtypeP t1 t2 → r.relation ∷ t2 :=
   by
     intro h1
-    apply subtypeC_subtype at h1
+    apply subtypeP_subtype at h1
     dsimp [subtype] at h1
     apply h1
     cases r with | mk relation type_pf => apply type_pf
 
-  def cast {t1 : RelType R arity} (r : Rel t1) {t2 : RelType R arity} (subtype_pf : subtypeC t1 t2)
+  def cast {t1 : RelType R arity} (r : Rel t1) {t2 : RelType R arity} (subtype_pf : subtypeP t1 t2)
   : (Rel t2)
   := Rel.mk
     (r.relation)
     (castable r t2 subtype_pf)
 
   macro "cast" varName:ident : term
-    => do `((Subtype.cast $(varName) (by dsimp[subtypeC,RelType_arity_cast,subtypeC_same_arity])))
+    => do `((Subtype.cast $(varName) (by sorry)))
 
   macro "cast" varName:ident "∷" typeName:typeExpr: term
-    => do `((Subtype.cast $(varName) (by dsimp[subtypeC,RelType_arity_cast,subtypeC_same_arity]) : ∷ $(typeName)))
+    => do `((Subtype.cast $(varName) (by sorry) : ∷ $(typeName)))
 end Subtype
 
 section test_cast
@@ -111,44 +81,14 @@ section test_cast
   variable (FRAU : ∷ set PERSON)
   variable (m : ∷ lone PERSON)
 
-
-example : (Subtype.subtypeC (MANN).getType (PERSON).getType) = true
+example : (Subtype.subtypeP MANN.getType PERSON.getType)
 := by
-    dsimp[
-      Subtype.subtypeC,
-      Subtype.RelType_arity_cast,
-      Subtype.subtypeC_same_arity,
-      Rel.getType,
-      RelType.mk.unary_rel,
-      RelType.mk.rel]
-
-
-
-#check PERSON.getType
-#check m.getType
-  lemma l1 : Subtype.subtypeC (MANN).getType (PERSON).getType := by
     dsimp[Rel.getType]
-    dsimp[
-      Subtype.subtypeC,
-      Subtype.RelType_arity_cast,
-      Subtype.subtypeC_same_arity,
-      Rel.getType,
-      RelType.mk.unary_rel,
-      RelType.mk.rel]
-
-
-
-
-
-
-
-
 
   def m_cast :=
     (Subtype.cast
       m
-      (by
-        dsimp[Subtype.subtypeC,Subtype.RelType_arity_cast,Subtype.subtypeC_same_arity])
+      (by sorry)
       : ∷ some univ
     )
 
@@ -193,7 +133,7 @@ end test_cast
   instance {R : Type} [TupleSet R] {arity : ℕ} (t1 t2 : RelType R arity) (r : (Rel t1)):
     CoeDep (Rel t1) r (Rel t2) where
 --    coe := mkSupertype t1 t2 r
-    coe := Rel.mk r.relation (@Subtype.castable R _ arity t1 r t2 (by simp))
+    coe := Rel.mk r.relation (@Subtype.castable R _ arity t1 r t2 (by sorry))
 
 section test_coercion
   variable (ThoR_TupleSet : Type) [TupleSet ThoR_TupleSet]
