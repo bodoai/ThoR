8c9007824843b2439810adb5e7d7c77898ac2a9d
Andreas Mayer <Andreas.Mayer@hs-rm.de>
2025-02-11 10:31:33 +0100

    delaboration for more cases

--- ThoR/Alloy/Delab/Shared/Relation/binRelOp.lean
+++ ThoR/Alloy/Delab/Shared/Relation/binRelOp.lean
@@ -4,24 +4,40 @@ Released under license as described in the file LICENSE.
 Authors: s. file CONTRIBUTORS
 -/
 
+/-
+This file contains delaboration for all binRelOps, meaning
+binary operations between relations.
+
+Note, that ,the dotjoin is also included, even tho it is
+defined in its own file in the alloy part (for precedence reasons)
+-/
+
 import Lean
 import ThoR.Relation.Set
 import ThoR.Alloy.Delab.DelaborationService
 
-open Lean PrettyPrinter Delaborator SubExpr Expr
+open Lean PrettyPrinter Delaborator SubExpr
 
 -- TODO In welchem Namespace sollen die ThoR-spezifischen
 --      Typklassen definiert werden?
 --      - root
 --      - ThoR
 --      Nach getroffener Entscheidung: einheitlich handhaben.
-set_option linter.cdot false in
+set_option linter.cdot false in -- supress warning for use of .
 @[app_unexpander ThoR.Dotjoin.dotjoin]
 def unexpDotjoin : Unexpander
-  | `($_ $a $b) => `($a . $b)
+  | `($_ $a:ident $b:ident) => do
+
+    let new_a :=
+      delaborationService.switch_thoR_representation_to_alloy_representation a
+    let new_b :=
+      delaborationService.switch_thoR_representation_to_alloy_representation b
+
+    `($new_a . $new_b)
+
   | _ => throw Unit.unit
 
-set_option linter.cdot false in
+set_option linter.cdot false in -- supress warning for use of .
 @[app_unexpander ThoR.HDotjoin.hDotjoin]
 def unexpHDotjoin : Unexpander
   | `($_ $a:ident $b:ident) => do
@@ -45,5 +61,70 @@ def unexpHAdd : Unexpander
       delaborationService.switch_thoR_representation_to_alloy_representation b
 
     `($new_a + $new_b)
+
+  | _ => throw Unit.unit
+
+@[app_unexpander ThoR.HIntersect.hIntersect]
+def unexpHIntersect : Unexpander
+  | `($_ $a:ident $b:ident) => do
+
+    let new_a :=
+      delaborationService.switch_thoR_representation_to_alloy_representation a
+    let new_b :=
+      delaborationService.switch_thoR_representation_to_alloy_representation b
+
+    `($new_a & $new_b)
+
+  | _ => throw Unit.unit
+
+@[app_unexpander HSub.hSub]
+def unexpHSub : Unexpander
+  | `($_ $a:ident $b:ident) => do
+
+    let new_a :=
+      delaborationService.switch_thoR_representation_to_alloy_representation a
+    let new_b :=
+      delaborationService.switch_thoR_representation_to_alloy_representation b
+
+    `($new_a - $new_b)
+
+  | _ => throw Unit.unit
+
+@[app_unexpander HAppend.hAppend]
+def unexpHAppend : Unexpander
+  | `($_ $a:ident $b:ident) => do
+
+    let new_a :=
+      delaborationService.switch_thoR_representation_to_alloy_representation a
+    let new_b :=
+      delaborationService.switch_thoR_representation_to_alloy_representation b
+
+    `($new_a ++ $new_b)
+
+  | _ => throw Unit.unit
+
+@[app_unexpander ThoR.HDomRestr.hDomrestr]
+def unexpHDomRestr : Unexpander
+  | `($_ $a:ident $b:ident) => do
+
+    let new_a :=
+      delaborationService.switch_thoR_representation_to_alloy_representation a
+    let new_b :=
+      delaborationService.switch_thoR_representation_to_alloy_representation b
+
+    `($new_a <: $new_b)
+
+  | _ => throw Unit.unit
+
+@[app_unexpander ThoR.HRangeRestr.hRangerestr]
+def unexpHRangeRestr : Unexpander
+  | `($_ $a:ident $b:ident) => do
+
+    let new_a :=
+      delaborationService.switch_thoR_representation_to_alloy_representation a
+    let new_b :=
+      delaborationService.switch_thoR_representation_to_alloy_representation b
+
+    `($new_a :> $new_b)
 
   | _ => throw Unit.unit
