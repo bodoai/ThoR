e8e8bf8f9f85d7aae10fd1827aa3f07b78b825b9
Andreas Mayer <Andreas.Mayer@hs-rm.de>
2025-02-11 10:32:14 +0100

    organize imports

---
+++ ThoR/Alloy/Delab/Shared.lean
@@ -0,0 +1,10 @@
+/-
+Copyright (c) 2024 RheinMain University of Applied Sciences
+Released under license as described in the file LICENSE.
+Authors: s. file CONTRIBUTORS
+-/
+
+import ThoR.Alloy.Delab.Shared.mult
+
+import ThoR.Alloy.Delab.Shared.Logic
+import ThoR.Alloy.Delab.Shared.Relation
---
+++ ThoR/Alloy/Delab/Shared/Relation.lean
@@ -0,0 +1,9 @@
+/-
+Copyright (c) 2024 RheinMain University of Applied Sciences
+Released under license as described in the file LICENSE.
+Authors: s. file CONTRIBUTORS
+-/
+
+import ThoR.Alloy.Delab.Shared.Relation.binRelOp
+import ThoR.Alloy.Delab.Shared.Relation.unRelOp
+import ThoR.Alloy.Delab.Shared.Relation.unRelBoolOp
