46a3bf9cf76c5a35486e038c85bddc4eb8758d49
igler <bodo.igler@hs-rm.de>
2025-02-19 21:52:58 +0100

    completed RelType' and RelType

--- ThoR/Relation/RelType.lean
+++ ThoR/Relation/RelType.lean
@@ -25,17 +25,42 @@ inductive RelType' (R : Type) [TupleSet R] : Type :=
   | complex
     (t1 : RelType' R) (m1 m2 : Shared.mult) (t2 : RelType' R) : RelType' R
   | intersect (t1 : RelType' R) (t2 : RelType' R) : RelType' R
+  | add (t1 : RelType' R) (t2 : RelType' R): RelType' R
+  | sub (t1 : RelType' R) (t2 : RelType' R):
+      RelType' R
+  | append (t1 : RelType' R) (t2 : RelType' R):
+      RelType' R
+  | cartprod (t1 : RelType' R) (t2 : RelType' R):
+      RelType' R
   | dotjoin (t1 : RelType' R) (t2 : RelType' R) :  RelType' R
+  | transclos (t : RelType' R):
+      RelType' R
+  | reftransclos (t : RelType' R):
+      RelType' R
+  | transpose (t : RelType' R):
+      RelType' R
+  | domrestr (t1 : RelType' R) (t2 : RelType' R):
+      RelType' R
+  | rangerestr (t1 : RelType' R) (t2 : RelType' R):
+      RelType' R
+
 
 -- Why define checkArityEq as a macro and not as a function?
 -- Putting this into a macro makes it easier to implement RelType'.arity.
 -- Putting this into a function requires an explicit proof for termination of arity.
-macro "checkArityEq" "(" f:term ", " t1:term ", " t2:term ")": term =>
+macro "checkArityEq" "(" arity:term ", " t1:term ", " t2:term ")": term =>
 `(do
-    let n1 ← ($f $t1)
-    let n2 ← ($f $t2)
+    let n1 ← ($arity $t1)
+    let n2 ← ($arity $t2)
     if (n1 = n2)
     then return n1
+    else none)
+
+macro "checkArityN" "(" arity:term ", " t:term ", " n:term ")": term =>
+`(do
+    let tn ← ($arity $t)
+    if (tn = $n)
+    then return $n
     else none)
 
 @[simp]
@@ -47,30 +72,27 @@ def RelType'.arity {R : Type} [TupleSet R] (t : RelType' R) :=
   | constant _ n _    => some n
   | complex t1 _ _ t2 => Nat.add <*> t1.arity <*> t2.arity
   | intersect t1 t2   => checkArityEq (arity, t1, t2)
+  | add t1 t2   => checkArityEq (arity, t1, t2)
+  | sub t1 t2   => checkArityEq (arity, t1, t2)
+  | append t1 t2   => checkArityEq (arity, t1, t2)
+  | cartprod t1 t2 => Nat.add <*> t1.arity <*> t2.arity
   | dotjoin t1 t2 => do
                       let n1 ← t1.arity
                       let n2 ← t2.arity
                       if (n1 > 0 ∧ n2 >0)
                       then return n1 + n2 - 2
                       else none
-
-  -- | add (t1 : RelType' R) (t2 : RelType' R): RelType' R
-  -- | sub (t1 : RelType R n) (t2 : RelType R n):
-  --     RelType R n
-  -- | append (t1 : RelType R n) (t2 : RelType R n):
-  --     RelType R n
-  -- | cartprod (t1 : RelType R n1) (t2 : RelType R n2):
-  --     RelType R (n1 + n2)
-  -- | transclos (t : RelType R 2):
-  --     RelType R 2
-  -- | reftransclos (t : RelType R 2):
-  --     RelType R 2
-  -- | transpose (t : RelType R 2):
-  --     RelType R 2
-  -- | domrestr (t1 : RelType R 1) (t2 : RelType R n):
-  --     RelType R n
-  -- | rangerestr (t1 : RelType R n) (t2 : RelType R 1):
-  --     RelType R n
+  | transclos t => checkArityN (arity, t, 2)
+  | reftransclos t => checkArityN (arity, t, 2)
+  | transpose t => checkArityN (arity, t, 2)
+  | domrestr t1 t2 => do
+                        let _ ← checkArityN (arity, t1, 1)
+                        let n2 ← t2.arity
+                        return n2
+  | rangerestr t1 t2 => do
+                        let n1 ← t1.arity
+                        let _ ← checkArityN (arity, t2, 1)
+                        return n1
 
 def RelType (R : Type) [TupleSet R] (n : ℕ):= { r : RelType' R // r.arity = some n }
 
@@ -104,51 +126,40 @@ def RelType.complex {R : Type} [TupleSet R] {n1 : ℕ} (t1 : RelType' R) (h1 : t
 instance (R : Type) [TupleSet R] (n : ℕ) : Intersect (RelType R n) where
   intersect t1 t2 := Subtype.mk (RelType'.intersect t1.1 t2.1) (by simp)
 
+instance (R : Type) [TupleSet R] (n : ℕ) : Add (RelType R n) where
+  add t1 t2 := Subtype.mk (RelType'.add t1.1 t2.1) (by simp)
+
+instance (R : Type) [TupleSet R] (n : ℕ) : Sub (RelType R n) where
+  sub t1 t2 := Subtype.mk (RelType'.sub t1.1 t2.1) (by simp)
+
+instance (R : Type) [TupleSet R] (n : ℕ) : Append (RelType R n) where
+  append t1 t2 := Subtype.mk (RelType'.append t1.1 t2.1) (by simp)
+
+instance (R : Type) [TupleSet R] (n1 n2 : ℕ):
+    HCartprod (RelType R n1) (RelType R n2) (outParam (RelType R (n1 + n2))) where
+      hCartprod (t1 : RelType R n1) (t2 : RelType R n2) :=
+        Subtype.mk (RelType'.cartprod t1.1 t2.1) (by simp)
+
 instance (R : Type) [TupleSet R] (n1 n2 : ℕ):
     HDotjoin (RelType R (n1 + 1)) (RelType R (n2 + 1)) (outParam (RelType R (n1 + n2))) where
       hDotjoin (t1 : RelType R (n1 +1)) (t2 : RelType R (n2 + 1)):=
         Subtype.mk (RelType'.dotjoin t1.1 t2.1) (by simp ; ring_nf ; rw[add_assoc, add_comm] ; simp)
 
--- macro "op_inst" name:ident typeClass:ident op:ident : command
--- => do
---     `(
---     instance (R : Type) [TupleSet R] (n : ℕ):
---         $typeClass (RelType R n) where
---             $op := $name
---     )
--- op_inst RelType.intersect Intersect intersect
--- op_inst RelType.add Add add
--- op_inst RelType.sub Sub sub
--- op_inst RelType.append Append append
+instance (R : Type) [TupleSet R] : Transclos (RelType R 2) where
+  transclos t := Subtype.mk (RelType'.transclos t.1) (by simp)
+instance (R : Type) [TupleSet R] : ReflTransclos (RelType R 2) where
+  rtransclos t := Subtype.mk (RelType'.transclos t.1) (by simp)
+instance (R : Type) [TupleSet R] : Transpose (RelType R 2) where
+  transpose t := Subtype.mk (RelType'.transpose t.1) (by simp)
 
--- instance (R : Type) [TupleSet R] (n1 n2 : ℕ):
---     HCartprod (RelType R n1) (RelType R n2) (outParam (RelType R (n1 + n2))) where
---         hCartprod (t1 : RelType R n1) (t2 : RelType R n2):=
---         RelType.cartprod t1 t2
-
--- instance (R : Type) [TupleSet R] (n1 n2 : ℕ):
---     HDotjoin (RelType R (n1 +1)) (RelType R (n2+1)) (outParam (RelType R (n1 + n2))) where
---         hDotjoin (t1 : RelType R (n1 +1)) (t2 : RelType R (n2 + 1)):=
---         RelType.dotjoin t1 t2
-
--- instance (R : Type) [TupleSet R]: Transclos (RelType R 2) where
---         transclos := RelType.transclos
--- instance (R : Type) [TupleSet R]: ReflTransclos (RelType R 2) where
---         rtransclos := RelType.reftransclos
--- instance (R : Type) [TupleSet R]: Transpose (RelType R 2) where
---         transpose := RelType.transpose
-
--- macro "unop_inst" name:ident typeClass:ident op:ident : command
--- => do
---     `(
---     instance (R : Type) [TupleSet R] (n : ℕ):
---         $typeClass (RelType R n) where
---             $op := $name
---     )
-
--- instance (R : Type) [TupleSet R] (n : ℕ):
---     Add (RelType R n) where
---         add := RelType.add
+instance (R : Type) [TupleSet R] (n : ℕ):
+    HDomRestr (RelType R 1) (RelType R n) (outParam (RelType R n)) where
+      hDomrestr (t1 : RelType R 1) (t2 : RelType R n) :=
+        Subtype.mk (RelType'.domrestr t1.1 t2.1) (by simp)
+instance (R : Type) [TupleSet R] (n : ℕ):
+    HRangeRestr (RelType R n) (RelType R 1) (outParam (RelType R n)) where
+      hRangerestr (t1 : RelType R n) (t2 : RelType R 1) :=
+        Subtype.mk (RelType'.rangerestr t1.1 t2.1) (by simp)
 
 instance [TupleSet R] {n : ℕ}: Arity (RelType R n) where
   arity             := n
