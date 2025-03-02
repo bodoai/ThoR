db5f67fbf10fa52e47b51edb3526f68f68be49a6
igler <bodo.igler@hs-rm.de>
2025-03-02 21:20:05 +0100

    rough idea for rewrite-Makro

--- ThoR/Test/Alloy/grandpa2.lean
+++ ThoR/Test/Alloy/grandpa2.lean
@@ -50,6 +50,57 @@ end
 
 startTestBlock language.grandpa1
 
+syntax "dup" term "as" ident : tactic
+macro_rules
+  | `(tactic|dup $t:term as $hypothesis:ident) =>
+  `(tactic|
+    have $hypothesis : $t = $t := by simp)
+
+syntax "dup_rel_r" term "as" ident : tactic
+macro_rules
+  | `(tactic|dup_rel_r $t:term as $hypothesis:ident) =>
+  `(tactic|
+    dup $t as $hypothesis
+    ;
+    conv at $hypothesis =>
+      rhs
+      simp [ThoR.HSubset.hSubset]
+      simp [ThoR.Rel.subset]
+      simp [ThoR.HDotjoin.hDotjoin]
+      dsimp [HAdd.hAdd]
+    )
+
+syntax "dup_rel_l" term "as" ident : tactic
+macro_rules
+  | `(tactic|dup_rel_l $t:term as $hypothesis:ident) =>
+  `(tactic|
+    dup $t as $hypothesis
+    ;
+    conv at $hypothesis =>
+      lhs
+      simp [ThoR.HSubset.hSubset]
+      simp [ThoR.Rel.subset]
+      simp [ThoR.HDotjoin.hDotjoin]
+      dsimp [HAdd.hAdd]
+    )
+
+syntax "rewrite" term "to" term "as" ident : tactic
+macro_rules
+  | `(tactic|rewrite $t1:term to $t2:term as $h:ident) =>
+  `(tactic|
+    dup_rel_r $t1 as h1
+    ;
+    dup_rel_l $t2 as h2
+    ;
+    rw [Rules.dotjoin.add.dist.r] at h1
+    ;
+    have $h := h2.mp ∘ h1.mp
+    ;
+    clear h1 h2
+  )
+
+
+
 lemma l1 : ∻ language.grandpa1.asserts.NoSelfGrandpa := by
   unfold NoSelfGrandpa
   apply Rules.no.intro
@@ -60,38 +111,45 @@ lemma l1 : ∻ language.grandpa1.asserts.NoSelfGrandpa := by
   intro contra
   simp [ThoR.Quantification.Formula.eval] at contra
 
-  have h1 :
-    p ⊂ p ⋈ (((∻ Person.mother) + (∻ Person.father)) ⋈ ((∻ Person.father)))
-    ↔
-    p ⊂ p ⋈ (((∻ Person.mother) + (∻ Person.father)) ⋈ ((∻ Person.father)))
-  := by simp
-  have h2 :
-    p ⊂ p ⋈ (((∻ Person.mother) ⋈ (∻ Person.father)) + ((∻ Person.father)) ⋈ ((∻ Person.father)))
-    ↔
-    p ⊂ p ⋈ (((∻ Person.mother) ⋈ (∻ Person.father)) + ((∻ Person.father)) ⋈ ((∻ Person.father)))
-  := by simp
+  dup_rel_r p ⊂ p ⋈ (((∻ Person.mother) + (∻ Person.father)) ⋈ ((∻ Person.father))) as h1
 
-  conv at h1 =>
-    rhs
-    simp [ThoR.HSubset.hSubset]
-    simp [ThoR.Rel.subset]
-    simp [ThoR.HDotjoin.hDotjoin]
-    dsimp [HAdd.hAdd]
+  dup_rel_l p ⊂ p ⋈ (((∻ Person.mother) ⋈ (∻ Person.father)) + ((∻ Person.father)) ⋈ ((∻ Person.father))) as h2
 
-  conv at h2 =>
-    lhs
-    simp [ThoR.HSubset.hSubset]
-    simp [ThoR.Rel.subset]
-    simp [ThoR.HDotjoin.hDotjoin]
-    dsimp [HAdd.hAdd]
 
-  rw [Rules.dotjoin.add.dist.r] at h1
+  -- have h1 :
+  --   p ⊂ p ⋈ (((∻ Person.mother) + (∻ Person.father)) ⋈ ((∻ Person.father)))
+  --   ↔
+  --   p ⊂ p ⋈ (((∻ Person.mother) + (∻ Person.father)) ⋈ ((∻ Person.father)))
+  -- := by simp
+  -- have h2 :
+  --   p ⊂ p ⋈ (((∻ Person.mother) ⋈ (∻ Person.father)) + ((∻ Person.father)) ⋈ ((∻ Person.father)))
+  --   ↔
+  --   p ⊂ p ⋈ (((∻ Person.mother) ⋈ (∻ Person.father)) + ((∻ Person.father)) ⋈ ((∻ Person.father)))
+  -- := by simp
 
-  have hr := h2.mp ∘ h1.mp
+  -- conv at h1 =>
+  --   rhs
+  --   simp [ThoR.HSubset.hSubset]
+  --   simp [ThoR.Rel.subset]
+  --   simp [ThoR.HDotjoin.hDotjoin]
+  --   dsimp [HAdd.hAdd]
 
-  apply h1.mp at contra
-  apply h2.mp at contra
+  -- conv at h2 =>
+  --   lhs
+  --   simp [ThoR.HSubset.hSubset]
+  --   simp [ThoR.Rel.subset]
+  --   simp [ThoR.HDotjoin.hDotjoin]
+  --   dsimp [HAdd.hAdd]
 
+  -- rw [Rules.dotjoin.add.dist.r] at h1
+
+  -- have hr := h2.mp ∘ h1.mp
+
+  -- apply h1.mp at contra
+  -- apply h2.mp at contra
+
+  rewrite p ⊂ p ⋈ (((∻ Person.mother) + (∻ Person.father)) ⋈ ((∻ Person.father))) to p ⊂ p ⋈ (((∻ Person.mother) ⋈ (∻ Person.father)) + ((∻ Person.father)) ⋈ ((∻ Person.father))) as hr
+  clear h1 h2
 
   -- fact f0 : language.grandpa1.facts.f0
   -- sorry
