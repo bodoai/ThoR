64a2a755d8efbe5d5ca3bb4ed0323b8dbc82562e
igler <bodo.igler@hs-rm.de>
2025-03-04 16:09:51 +0100

    idea for rewrite r.relation to r

--- ThoR/Test/Alloy/grandpa2.lean
+++ ThoR/Test/Alloy/grandpa2.lean
@@ -108,7 +108,8 @@ elab " rewrite " rw_target:ident " to " rw_result:term  : tactic =>
             ]
           ]
 
-        let h1_new ← Lean.Core.mkFreshUserName `h
+        let h1_new := "h1".toName
+--        let h1_new ← Lean.Core.mkFreshUserName `h
         liftMetaTactic fun mvarId => do
           let mvarIdNew ← mvarId.assert h1_new declTypeDup declTypeDupProof
           let (_, mvarIdNew) ← mvarIdNew.intro h1_new
@@ -121,24 +122,46 @@ elab " rewrite " rw_target:ident " to " rw_result:term  : tactic =>
           simp [ThoR.HDotjoin.hDotjoin]
           dsimp [HAdd.hAdd]))
 
-        Lean.Elab.Tactic.withMainContext do
-          let ctx ← Lean.MonadLCtx.getLCtx -- get the local context.
-          ctx.forM fun decl: Lean.LocalDecl => do
-            let declExpr := decl.toExpr -- Find the expression of the declaration.
-            let declName := decl.userName -- Find the name of the declaration.
-            let declType ← Lean.Meta.inferType declExpr -- **NEW:** Find the type.
-            if declName = h1_new
-            then
-              dbg_trace f!"h1_new: {declType}"
+        -- Lean.Elab.Tactic.withMainContext do
+        --   let ctx ← Lean.MonadLCtx.getLCtx -- get the local context.
+        --   ctx.forM fun decl: Lean.LocalDecl => do
+        --     let declExpr := decl.toExpr -- Find the expression of the declaration.
+        --     let declName := decl.userName -- Find the name of the declaration.
+        --     let declType ← Lean.Meta.inferType declExpr -- **NEW:** Find the type.
+        --     if declName = h1_new
+        --     then
+        --       dbg_trace f!"h1_new: {declType}"
 
-          let h2_new ← Lean.Core.mkFreshUserName `h
-          Lean.Elab.Tactic.evalTactic (← `(tactic| dup_rel_l $rw_result:term as $(mkIdent h2_new):ident))
+        let h2_new ← Lean.Core.mkFreshUserName `h
+        Lean.Elab.Tactic.evalTactic (← `(tactic| dup_rel_l $rw_result:term as $(mkIdent h2_new):ident))
 
-          Lean.Elab.Tactic.evalTactic (← `(tactic| rewrite [Rules.dotjoin.add.dist.r] at $(mkIdent h1_new):ident))
+        Lean.Elab.Tactic.evalTactic (← `(tactic| rewrite [Rules.dotjoin.add.dist.r] at $(mkIdent h1_new):ident))
 
-          Lean.Elab.Tactic.evalTactic (← `(tactic| apply (($(mkIdent h2_new)).mp ∘ ($(mkIdent h1_new)).mp) at $rw_target:ident))
+        Lean.Elab.Tactic.evalTactic (← `(tactic| apply (($(mkIdent h2_new)).mp ∘ ($(mkIdent h1_new)).mp) at $rw_target:ident))
 
-          Lean.Elab.Tactic.evalTactic (← `(tactic| clear $(mkIdent h1_new) $(mkIdent h2_new)))
+--        Lean.Elab.Tactic.evalTactic (← `(tactic| clear $(mkIdent h1_new) $(mkIdent h2_new)))
+
+
+lemma lift_add (R : Type) [ThoR.TupleSet R] {n : ℕ} {t1 t2 : ThoR.RelType R n} {a : ThoR.Rel t1} {b : ThoR.Rel t2} : a.relation + b.relation = (a + b).relation := by
+  dsimp [HAdd.hAdd]
+
+lemma lift_add' (R : Type) [ThoR.TupleSet R] {n : ℕ} {t1 t2 : ThoR.RelType R n} {a : ThoR.Rel t1} {b : ThoR.Rel t2} : Add.add a.relation b.relation = (a + b).relation := by
+  dsimp [HAdd.hAdd]
+
+lemma lift_subset (R : Type) [ThoR.TupleSet R] {n : ℕ} {t1 t2 : ThoR.RelType R n} {a : ThoR.Rel t1} {b : ThoR.Rel t2} : a.relation ⊂ b.relation ↔ a ⊂ b := by
+  dsimp [ThoR.HSubset.hSubset]
+  dsimp [ThoR.Rel.subset]
+  trivial
+
+lemma lift_subset' (R : Type) [ThoR.TupleSet R] {n : ℕ} {t1 t2 : ThoR.RelType R n} {a : ThoR.Rel t1} {b : ThoR.Rel t2} : ThoR.Subset.subset a.relation b.relation ↔ a ⊂ b := by
+  dsimp [ThoR.HSubset.hSubset]
+  dsimp [ThoR.Rel.subset]
+  trivial
+
+lemma lift_dotjoin (R : Type) [ThoR.TupleSet R] {n1 n2 : ℕ} {t1 : ThoR.RelType R (n1 + 1)} {t2 : ThoR.RelType R (n2 + 1)} {a : ThoR.Rel t1} {b : ThoR.Rel t2} : a.relation ⋈ b.relation = (a ⋈ b).relation := by
+  dsimp [ThoR.HDotjoin.hDotjoin]
+
+#check ThoR.Set.toSubset
 
 lemma l1 : ∻ language.grandpa1.asserts.NoSelfGrandpa := by
   unfold NoSelfGrandpa
@@ -151,6 +174,12 @@ lemma l1 : ∻ language.grandpa1.asserts.NoSelfGrandpa := by
   simp [ThoR.Quantification.Formula.eval] at contra
 
   rewrite contra to p ⊂ p ⋈ (((∻ Person.mother) ⋈ (∻ Person.father)) + ((∻ Person.father)) ⋈ ((∻ Person.father)))
+
+  rw [lift_dotjoin] at h1
+  rw [lift_dotjoin] at h1
+  rw [lift_add'] at h1
+  rw [lift_dotjoin] at h1
+  unfold ThoR.Subset.subset at h1
 
   sorry
 
