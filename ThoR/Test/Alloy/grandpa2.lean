4bc2cdada97316a319b919d8ad21e97f0670957a
igler <bodo.igler@hs-rm.de>
2025-03-07 13:29:55 +0100

    updated TODOs

--- ThoR/Test/Alloy/grandpa2.lean
+++ ThoR/Test/Alloy/grandpa2.lean
@@ -2,7 +2,7 @@ import ThoR.Alloy
 import ThoR.Test.Alloy.test_macro
 import ThoR.Rules
 
--- import ThoR.Alloy.Delab
+import ThoR.Alloy.Delab
 
 alloy
 module language/grandpa1 ---- Page 84, 85
@@ -54,35 +54,41 @@ open language.grandpa1
 lemma l1 : ∻ language.grandpa1.asserts.NoSelfGrandpa := by
   unfold NoSelfGrandpa
   apply Rules.no.intro
+--  intro contra
   apply Rules.some.neg -- TODO get rid of insertion of redundant Formula.prop in Rules.some.neg
+  -- TODO apply Rules.all.intro; intro p; unfold ThoR.Quantification.Formula.eval -> in one macro
   apply Rules.all.intro
   intro p
   unfold ThoR.Quantification.Formula.eval
-  intro contra
+  intro contra -- TODO proper intro macro that unfolds eval
+  -- TODO alternative: unfold not necessary if
+  --      appropriate coercion available
   unfold ThoR.Quantification.Formula.eval at contra
 
 --  thor_rw [Rules.dotjoin.add.dist.r] at contra
 
   fact f0 : language.grandpa1.facts.f0
-  cases f0 with
-  | intro f1 f2 =>
-    clear f2
-    apply Rules.no.elim at f1
-    apply f1
-    clear f1
-    apply Rules.some.intro p
-    have h1 : p ⊂ p ⋈ ((∻ this_φ_Person_ξ_mother + ∻ this_φ_Person_ξ_father) ⋈ (∻ this_φ_Person_ξ_mother + ∻ this_φ_Person_ξ_father)) := by
-      apply Rules.subset.trans
-      apply contra
-      apply Rules.dotjoin.subset.r
-      apply Rules.dotjoin.subset.r
-      apply Rules.add.subset.l
-    unfold ThoR.Quantification.Formula.eval
+  have ⟨f1, f2⟩ := f0
+  -- TODO and elimination
+  --      apply Rules.and.elim at f0 with f1 and f2?
+  clear f0
+  clear f2
+  apply Rules.no.elim at f1
+  apply f1
+  clear f1
+  apply Rules.some.intro p -- TODO include eval as step in intro
+  unfold ThoR.Quantification.Formula.eval
+  have h1 : p ⊂ p ⋈ ((∻ this_φ_Person_ξ_mother + ∻ this_φ_Person_ξ_father) ⋈ (∻ this_φ_Person_ξ_mother + ∻ this_φ_Person_ξ_father)) := by
+  -- TODO have h1 : [alloy| p in p.(mother + ...)... ] := by
     apply Rules.subset.trans
-    apply h1
+    apply contra
     apply Rules.dotjoin.subset.r
-    apply Rules.dotjoin.transclos_2
+    apply Rules.dotjoin.subset.r
+    apply Rules.add.subset.l
+  apply Rules.subset.trans
+  apply h1
+  apply Rules.dotjoin.subset.r
+  apply Rules.dotjoin.transclos_2
 
--- TODO
--- this_φ_Person_ξ_mother kann nicht gemischt werden mit /
--- umgewandelt werden in vars.Person.mother
+-- TODO get rid of:
+-- - (long) case names after introduction of new goal
