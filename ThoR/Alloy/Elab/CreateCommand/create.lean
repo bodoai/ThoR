6a4fb3e09075018f734da2cfa2f6523508bb1d29
Andreas Mayer <Andreas.Mayer@hs-rm.de>
2025-04-07 10:45:57 +0200

    end opened namespace

--- ThoR/Alloy/Elab/CreateCommand/create.lean
+++ ThoR/Alloy/Elab/CreateCommand/create.lean
@@ -13,86 +13,88 @@ open Lean Lean.Elab Command Term
 
 namespace Alloy
 
-syntax (name := creationSyntax) ("#" <|> "~")? "create" separatedNamespace : command
+  syntax (name := creationSyntax) ("#" <|> "~")? "create" separatedNamespace : command
 
-private def evaluateCreationCommand
-  (ident : Ident)
-  (logging : Bool)
-  : CommandElabM Unit := do
-    let monadeState ← get
+  private def evaluateCreationCommand
+    (ident : Ident)
+    (logging : Bool)
+    : CommandElabM Unit := do
+      let monadeState ← get
 
-    let dataName : Name := s!"{ident.getId.toString}_Data".toName
-    let ads := getAlloyData monadeState.env
+      let dataName : Name := s!"{ident.getId.toString}_Data".toName
+      let ads := getAlloyData monadeState.env
 
-    if let Option.some (ad : alloyData) := ads.find? dataName then
-      if logging then
-        logInfo s!"Module data for {ident.getId.toString} found:\n\n {ad}"
+      if let Option.some (ad : alloyData) := ads.find? dataName then
+        if logging then
+          logInfo s!"Module data for {ident.getId.toString} found:\n\n {ad}"
 
-      let st := ad.st
-      let ast := ad.ast
+        let st := ad.st
+        let ast := ad.ast
 
-      if !ast.modulVariables.isEmpty then
-        logError s!"The module you tried to create has unbound module variables \n\
-        ({ast.modulVariables})"
+        if !ast.modulVariables.isEmpty then
+          logError s!"The module you tried to create has unbound module variables \n\
+          ({ast.modulVariables})"
+
+        else
+
+          let except_commands := createCommands st
+          match except_commands with
+            | Except.error msg => throwError msg
+            | Except.ok commands =>
+                let mut commandString : String := ""
+
+                for command in commands do
+                  elabCommand command
+                  commandString := s!"{commandString} {command.raw.prettyPrint} \n\n"
+
+                if logging then
+                  logInfo commandString
+
+                let it :=  InheritanceTree.create ast
+                if logging then
+                  logInfo it.toString
+
+                let extensionAxiomCommands :=
+                  it.createInheritanceAxiomCommands
+                    (blockName := ident.getId)
+                    st.getSignatureNames st.getSignatureRNames
+
+                let mut extensionAxiomCommandsString := ""
+                for axiomCommand in extensionAxiomCommands do
+                  elabCommand axiomCommand
+                  extensionAxiomCommandsString :=
+                    s!"{extensionAxiomCommandsString} {axiomCommand.raw.prettyPrint} \n\n"
+
+                if logging then
+                  logInfo extensionAxiomCommandsString
 
       else
-
-        let except_commands := createCommands st
-        match except_commands with
-          | Except.error msg => throwError msg
-          | Except.ok commands =>
-              let mut commandString : String := ""
-
-              for command in commands do
-                elabCommand command
-                commandString := s!"{commandString} {command.raw.prettyPrint} \n\n"
-
-              if logging then
-                logInfo commandString
-
-              let it :=  InheritanceTree.create ast
-              if logging then
-                logInfo it.toString
-
-              let extensionAxiomCommands :=
-                it.createInheritanceAxiomCommands
-                  (blockName := ident.getId)
-                  st.getSignatureNames st.getSignatureRNames
-
-              let mut extensionAxiomCommandsString := ""
-              for axiomCommand in extensionAxiomCommands do
-                elabCommand axiomCommand
-                extensionAxiomCommandsString :=
-                  s!"{extensionAxiomCommandsString} {axiomCommand.raw.prettyPrint} \n\n"
-
-              if logging then
-                logInfo extensionAxiomCommandsString
-
-    else
-      logError s!"Cannot create {ident.getId.toString}, it does not exist."
+        logError s!"Cannot create {ident.getId.toString}, it does not exist."
 
 
-@[command_elab creationSyntax]
-private def creationImpl : CommandElab := fun stx => do
-  try
-    match stx with
-      | `(create $name:separatedNamespace) =>
-        evaluateCreationCommand
-          (separatedNamespace.toType name).representedNamespace
-          false
+  @[command_elab creationSyntax]
+  private def creationImpl : CommandElab := fun stx => do
+    try
+      match stx with
+        | `(create $name:separatedNamespace) =>
+          evaluateCreationCommand
+            (separatedNamespace.toType name).representedNamespace
+            false
 
-      | `(#create $name:separatedNamespace) =>
-        evaluateCreationCommand
-          (separatedNamespace.toType name).representedNamespace
-          true
+        | `(#create $name:separatedNamespace) =>
+          evaluateCreationCommand
+            (separatedNamespace.toType name).representedNamespace
+            true
 
-      | `(~create $name:separatedNamespace) =>
-        Lean.Elab.Command.failIfSucceeds
-          (
-            evaluateCreationCommand
-              (separatedNamespace.toType name).representedNamespace
-              false
-          )
+        | `(~create $name:separatedNamespace) =>
+          Lean.Elab.Command.failIfSucceeds
+            (
+              evaluateCreationCommand
+                (separatedNamespace.toType name).representedNamespace
+                false
+            )
 
-      | _ => return
-  catch | x => throwError x.toMessageData
+        | _ => return
+    catch | x => throwError x.toMessageData
+
+end Alloy
