1ff64d87c6c32bec2722d11169eb6172f53cc86c
Andreas Mayer <Andreas.Mayer@hs-rm.de>
2025-04-28 10:28:50 +0200

    syntax command that translatess to syntax

---
+++ ThoR/Alloy/Elab/SyntaxWithComments/syntax_with_comments.lean
@@ -0,0 +1,59 @@
+/-
+Copyright (c) 2024 RheinMain University of Applied Sciences
+Released under license as described in the file LICENSE.
+Authors: s. file CONTRIBUTORS
+-/
+import Lean
+
+open Lean Lean.Elab Command Term
+
+namespace Alloy
+
+  declare_syntax_cat syntax_with_comments_element
+
+  syntax str : syntax_with_comments_element
+  syntax ident : syntax_with_comments_element
+
+  syntax
+    (name := syntax_with_comments_stx)
+    "syntax_with_comments" (syntax_with_comments_element)* ":" ident : command
+
+  private def evaluate
+  (elements :TSyntaxArray `syntax_with_comments_element)
+  (stx_cat : Ident)
+  : Command
+  := Unhygienic.run do
+
+  let mut elements_as_stx : Array (TSyntax `stx) := #[]
+  for element in elements do
+    match element with
+      | `(syntax_with_comments_element | $str_element:str) =>
+        let str_element_stx ← `(stx | $(str_element):str)
+        elements_as_stx := elements_as_stx.push str_element_stx
+      | `(syntax_with_comments_element | $ident_element:ident) =>
+        let ident_element_stx ← `(stx | $(ident_element):ident)
+        elements_as_stx := elements_as_stx.push ident_element_stx
+      | _ => unreachable!
+
+  return ← `(command | syntax $[$elements_as_stx]* : $stx_cat)
+
+  @[command_elab syntax_with_comments_stx]
+  private def syntax_with_comments_implementation : CommandElab := fun stx => do
+    try
+      match stx with
+        | `(
+            syntax_with_comments
+            $elements:syntax_with_comments_element*
+            : $stx_cat:ident
+          ) =>
+
+          let s := (evaluate elements stx_cat)
+          logInfo s
+          elabCommand s
+
+        | _ => return
+    catch | x => throwError x.toMessageData
+
+end Alloy
+
+syntax_with_comments "a" : command
