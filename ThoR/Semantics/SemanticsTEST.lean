4fb934b76f3527f6bb4b6121a11ad4913ade634b
Andreas Mayer <Andreas.Mayer@hs-rm.de>
2025-05-13 10:58:15 +0200

    Semantics tmp TEST

---
+++ ThoR/Semantics/SemanticsTEST.lean
@@ -0,0 +1,50 @@
+import ThoR.Semantics.Semantics
+import ThoR.Relation.ElabCallMacro
+
+open ThoR ThoR.Semantics
+
+namespace  predtest
+
+  /-
+  sig a {}
+  sig b {}
+  -/
+  class  vars
+    (  ThoR_TupleSet  :  Type  )
+    [  ThoR.TupleSet  ThoR_TupleSet  ]  where
+      (  this_φ_a  :  Rel (RelType.mk.sig R Shared.mult.set) )
+      (  this_φ_b  :  Rel (RelType.mk.sig R Shared.mult.set) )
+
+  end  predtest
+
+  alias  predtest.vars.a  :=  predtest.vars.this_φ_a
+
+  alias  predtest.vars.b  :=  predtest.vars.this_φ_b
+
+  namespace  predtest.preds
+
+  variable
+    {  ThoR_TupleSet  :  Type  }
+    [  ThoR.TupleSet  ThoR_TupleSet  ]
+    [  predtest.vars  ThoR_TupleSet  ]
+
+  /-
+  pred p1 {
+    a in b
+  }
+  -/
+  def  p1
+    {  ThoR_TupleSet  :  Type  }
+    [  ThoR.TupleSet  ThoR_TupleSet  ]
+    [  predtest.vars  ThoR_TupleSet  ]  :=
+      Term.in
+        (Term.var
+          (Ty.expression
+            (RelType.mk.sig ThoR_TupleSet Shared.mult.set)
+          ) (  ∻  predtest.vars.this_φ_a  ))
+        (Term.var
+          (Ty.expression
+            (RelType.mk.sig ThoR_TupleSet Shared.mult.set)
+          ) (  ∻  predtest.vars.this_φ_b  ))
+
+end  predtest.preds
