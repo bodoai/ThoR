163c6a489eb7d13d29d1d4c412084fd75c243ff8
Andreas Mayer <Andreas.Mayer@hs-rm.de>
2025-06-05 10:28:43 +0200

    grouped args (in pred)

--- ThoR/Alloy/Delab/Shared/mutual/mutualTerm.lean
+++ ThoR/Alloy/Delab/Shared/mutual/mutualTerm.lean
@@ -63,6 +63,7 @@ def unexpTerm_union : Unexpander
 
   | _ => throw Unit.unit
 
+/-
 @[app_unexpander ThoR.Semantics.Term.lam]
 def unexp_lam : Unexpander
   | `($_ $lambda_function) =>
@@ -86,21 +87,72 @@ def unexp_lam : Unexpander
               | _ => throw Unit.unit
       | _ => throw Unit.unit
   | _ => throw Unit.unit
+-/
+
+private partial def getArgs
+  (body : TSyntax `term)
+  : (Array (TSyntax `delaborator_body)) × (TSyntax `delaborator_body) := Id.run do
+  let mut result := #[]
+  let mut trueBody := unhygienicUnfolder `(delaborator_body|$(mkIdent `default):ident)
+  match body with
+    | `(ThoR.Semantics.Term.lam $lambda_function) =>
+      match lambda_function with
+        | `(fun $lambda_variable ↦ $body) =>
+          match lambda_variable with
+            | `( Lean.Parser.Term.funBinder |
+                $(variable_nameTerm):term ) =>
+                  match variable_nameTerm with
+                    | `($variable_name:ident) =>
+                      let arg := unhygienicUnfolder `(delaborator_body|$variable_name:ident)
+                      result := result.push arg
+                    | _ => result := result
+
+          /- if the body is not a lam then its the final body -/
+          match body with
+            | `([alloy'| $body]) =>
+              trueBody := body
+            | _ =>
+              let subResult := (getArgs body)
+              result := result.append subResult.1
+              trueBody := subResult.2
+
+        | _ => result := result
+    | _ => result := result
+
+  (result, trueBody)
 
 @[app_unexpander ThoR.Semantics.Term.pred_def]
 def unexpTerm_predDef : Unexpander
-  | `($_ $name [alloy' | $body] ) => do
+  | `($_ $name $body ) => do
 
     let nn := match name with
       | `(term | $n:str) => n.getString
       | _ => unreachable!
 
-    let bb := unhygienicUnfolder
-      `( delaborator_body |
-        pred
-        $(mkIdent nn.toName)
-        $body
-      )
+    --panic! s!"{body}"
+
+    /-get args here to group them-/
+    let argsAndBody := getArgs body
+    let args := argsAndBody.1
+    let argsBody := unhygienicUnfolder `(delaborator_body | [$[$(args)],*])
+    let body := unhygienicUnfolder `(delaborator_body | { $(argsAndBody.2) } )
+
+    let bb :=
+      if args.isEmpty then
+        unhygienicUnfolder
+        `( delaborator_body |
+          pred
+          $(mkIdent nn.toName)
+          $body
+        )
+      else
+        unhygienicUnfolder
+          `( delaborator_body |
+            pred
+            $(mkIdent nn.toName)
+            $argsBody
+            $body
+          )
 
     `([alloy' | $bb:delaborator_body])
 
