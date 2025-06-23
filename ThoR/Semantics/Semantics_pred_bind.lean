403c06741f97e3d7238b546e58a0192e67397020
igler <bodo.igler@hs-rm.de>
2025-06-23 21:12:04 +0200

    idea for fix of pred/bind

---
+++ ThoR/Semantics/Semantics_pred_bind.lean
@@ -0,0 +1,36 @@
+  inductive Ty : Type → Type 1 where
+    | formula : {T : Type} → Ty T -- Prop
+    | pred : {T : Type} → Ty T
+
+  @[reducible]
+  def Ty.eval {T : Type} (ty : Ty T) : Type :=
+    match ty with
+    | .formula => Prop
+    | @Ty.pred T => T → Prop
+
+  inductive Term : {T : Type} → (ty : Ty T) → Type (u+1) where
+    | form : Prop → Term .formula
+    | pred {T : Type} : (T → Term .formula) → Term (@Ty.pred T)
+    | bind {T : Type} : Term (@Ty.pred T) → Term .formula
+
+@[reducible]
+def Term.eval {T : Type} {ty : Ty T} (t : Term ty) : ty.eval :=
+    match t with
+
+    | Term.form f => f
+
+    | .pred f => λ x => (f x).eval
+
+    | .bind t => ∀ x, t.eval x
+
+#check (@Term.bind Nat _ (@Term.pred Nat _ ( λ y => (@Term.bind Nat _ (@Term.pred Nat _ (λ (x : Nat) => @Term.form Nat (x = y))))))).eval
+
+example : (@Term.bind Nat _ (@Term.pred Nat _ ( λ y => (@Term.bind Nat _ (@Term.pred Nat _ (λ (x : Nat) => @Term.form Nat (x = y))))))).eval ↔ ∀ (x y : Nat), y = x:= by
+  dsimp[Term.eval]
+  apply Iff.intro
+  · intro h x y
+    specialize h x y
+    apply h
+  · intro h x y
+    specialize h x y
+    apply h
